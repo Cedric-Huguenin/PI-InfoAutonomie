727da8d5e76b48aad99ddad2d0f563638f25e3da
Cédric Huguenin <cedric.huguenin@telecomnancy.eu>
2015-01-26 18:00:27 +0100

    [+] Display raw value with morris

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,7 @@ import model.Event;
 import play.mvc.Controller;
 import play.mvc.Result;
 import utils.TimestampUtils;
+import views.html.events;
 import views.html.index;
 import views.html.raw_values;
 
@@ -89,6 +90,69 @@ public class Application extends Controller {
         }
 
         return ok(raw_values.render("Your new application is ready.", dataNode.getData(), events));
+    }
+
+    public static Result events() {
+        String url = "http://iotlab.telecomnancy.eu/rest/data/1/light1/24/153.111/1421920258/1422006659";
+        StringBuffer result = new StringBuffer();
+        DataNode dataNode = new DataNode();
+        ArrayList<Event> eventsList = null;
+        try {
+            URL oracle = new URL(url);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(oracle.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                result.append(inputLine);
+            System.out.println(inputLine);
+            in.close();
+
+            // map the json data in a DataNode
+            ObjectMapper mapper = new ObjectMapper();
+            dataNode = mapper.readValue(result.toString(), DataNode.class);
+
+            Collections.reverse(dataNode.getData());
+            boolean day = true;
+            boolean light = false;
+            double thresholdNightDay = 170, thresholdLight = 200;
+            eventsList = new ArrayList<>();
+            for(Data data : dataNode.getData()) {
+                double lightValue = data.getValue();
+                if(lightValue < thresholdNightDay) { // night level
+                    if (day || light) {
+                        eventsList.add(new Event(data.getTimestamp(), "back to night", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+                    } else {
+                        eventsList.add(new Event(data.getTimestamp(), "night", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+                    }
+                    day = false;
+                    light = false;
+                } else if(lightValue > thresholdNightDay && lightValue < thresholdLight) { // day level
+                    if(!day) {
+                        eventsList.add(new Event(data.getTimestamp(), "sunrise", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+                    } else if(light) {
+                        eventsList.add(new Event(data.getTimestamp(), "light turned off", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+
+                    } else {
+                        eventsList.add(new Event(data.getTimestamp(), "day", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+                    }
+                    day = true;
+                    light= false;
+                } else { // light is on
+                    if(!light) {
+                        eventsList.add(new Event(data.getTimestamp(), "light turned on", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+                    } else {
+                        eventsList.add(new Event(data.getTimestamp(), "light already on", TimestampUtils.timestampToString(data.getTimestamp()), lightValue));
+                    }
+                    light= true;
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ok(events.render("Your new application is ready.", eventsList));
     }
 
 }
