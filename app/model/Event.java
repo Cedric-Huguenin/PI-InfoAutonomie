7cf2086b1e84ba93874e87b6385e105a9381d358
Mathieu Morainville <mathieumorainville@hotmail.com>
2015-01-31 16:59:27 +0100

    [+] View to create en event

--- app/model/Event.java
+++ app/model/Event.java
@@ -6,8 +6,7 @@ import java.util.List;
  * Created by Ced on 31/01/2015.
  */
 public class Event {
-
     public List<BasicEvent> basicEvents;
     public int duration;
-    public  TimeInterval timeInterval;
+    public TimeInterval timeInterval;
 }
