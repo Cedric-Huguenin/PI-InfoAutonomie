5cc6da25e62acfbf74d301ca62043ebeb827a50c
Mathieu Morainville <mathieumorainville@hotmail.com>
2015-02-08 01:41:27 +0100

    [+] Basic events based on the delta between two values are working (/basic/data)

---
+++ app/controllers/BasicEventController.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.json.Data;
+import model.json.DataNode;
+import play.mvc.Result;
+import utils.TimestampUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static play.mvc.Results.ok;
+
+/**
+ * Created by Mathieu on 07/02/2015.
+ */
+public class BasicEventController {
+
+    public static Result data() {
+        DataNode rawDataNode = null;
+        String response = "";
+        String date = "";
+        long timestamp = 0;
+        double value = 0.0;
+        List<String> basicEventList = new ArrayList<String>();
+
+        // first, parse the data obtained from the url and put it in a DataNode
+        try {
+            URL url = new URL("http://iotlab.telecomnancy.eu/rest/data/1/light1/24/153.111");
+            ObjectMapper mapper = new ObjectMapper();
+            rawDataNode = mapper.readValue(url, DataNode.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // then, extract the timestamp and the values from the data to compute the basic events
+        if (rawDataNode != null) {
+            List<Data> rawDataList = rawDataNode.getData();
+            Collections.reverse(rawDataList);
+
+            for (Data rawData : rawDataList) {
+                double maxDifference = 30.0; // max 30 lux of difference for example
+                int actualIndex = rawDataList.indexOf(rawData);
+                timestamp = (long) rawData.getTimestamp();
+                value = rawData.getValue();
+
+                if (actualIndex + 1 < rawDataList.size()) {
+                    Data nextData = rawDataList.get(actualIndex + 1);
+                    double actualDifference = Math.abs(value - nextData.getValue());
+                    if (actualDifference > maxDifference) {
+                        date = TimestampUtils.formatToString(nextData.getTimestamp(), "dd-MM-yyyy HH:mm:ss");
+                        response = "BasicEvent occured at " + date + " (from " + value + " to " + nextData.getValue() + ")";
+                        basicEventList.add(response);
+                        System.out.println(response);
+                    }
+                }
+            }
+        } else {
+            response = "Aucun évènement n'a pu être généré car aucune donnée brute n'a été reçue.";
+        }
+
+        return ok(views.html.basic.data.render("Your new application is ready.", basicEventList));
+    }
+
+}
