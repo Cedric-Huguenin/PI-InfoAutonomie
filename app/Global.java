287ce6e3620b20e5fc8c2ff65c5309e37a4f57a6
Mathieu Morainville <mathieumorainville@hotmail.com>
2015-02-22 21:41:39 +0100

    [Fix] Scheduler working

--- app/Global.java
+++ app/Global.java
@@ -13,39 +13,38 @@ import java.util.concurrent.TimeUnit;
  */
 public class Global extends GlobalSettings {
 
-        @Override
-        public void onStart(Application application) {
+    @Override
+    public void onStart(Application application) {
+        Akka.system().scheduler().scheduleOnce(
+                Duration.create(5, TimeUnit.SECONDS),
+                () -> {
+                    Logger.info("ON START ---    " + System.currentTimeMillis());
+                    System.out.println("ON START ---    " + System.currentTimeMillis());
+                },
+                Akka.system().dispatcher()
+        );
 
-            Akka.system().scheduler().scheduleOnce(
-                    Duration.create(5, TimeUnit.SECONDS),
-                    () -> {
-                            Logger.info("ON START ---    " + System.currentTimeMillis());
-                            System.out.println("ON START ---    " + System.currentTimeMillis());
-                    },
-                    Akka.system().dispatcher()
-            );
-
-            Akka.system().scheduler().schedule(
-                    Duration.create(0, TimeUnit.MILLISECONDS), //Initial delay 0 milliseconds
-                    Duration.create(10, TimeUnit.SECONDS),     //Frequency 10 seconds
+        Akka.system().scheduler().schedule(
+                Duration.create(0, TimeUnit.MILLISECONDS), //Initial delay 0 milliseconds
+                Duration.create(10, TimeUnit.SECONDS),     //Frequency 10 seconds
 //                    Duration.create(nextExecutionInSeconds(8, 0), TimeUnit.SECONDS),
 //                    Duration.create(24, TimeUnit.HOURS),
-                    () -> {
-                            Logger.info("EVERY DAY AT 8:00 ---    " + System.currentTimeMillis());
-                            System.out.println("EVERY DAY ---    " + System.currentTimeMillis());
-                    },
-                    Akka.system().dispatcher()
-            );
-        }
+                () -> {
+                    Logger.info("EVERY 10 SECONDS ---    " + System.currentTimeMillis());
+                    System.out.println("EVERY 10 SECONDS ---    " + System.currentTimeMillis());
+                },
+                Akka.system().dispatcher()
+        );
+    }
 
-    public static int nextExecutionInSeconds(int hour, int minute){
+    public static int nextExecutionInSeconds(int hour, int minute) {
         return Seconds.secondsBetween(
                 new DateTime(),
                 nextExecution(hour, minute)
         ).getSeconds();
     }
 
-    public static DateTime nextExecution(int hour, int minute){
+    public static DateTime nextExecution(int hour, int minute) {
         DateTime next = new DateTime()
                 .withHourOfDay(hour)
                 .withMinuteOfHour(minute)
